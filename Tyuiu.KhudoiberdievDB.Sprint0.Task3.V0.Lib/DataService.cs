51ea4e81151b59c7568d29a502c9f0a7f7be188e
Tyuiu.KhudojberdievDB@outlook.com <Tyuiu.KhudojberdievDB@outlook.com>
2024-09-19 17:56:30 +0300

    Task3

---
+++ Tyuiu.KhudoiberdievDB.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Tyuiu.KhudoiberdievDB.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+        {  return a + b; } 
+    }
+}
