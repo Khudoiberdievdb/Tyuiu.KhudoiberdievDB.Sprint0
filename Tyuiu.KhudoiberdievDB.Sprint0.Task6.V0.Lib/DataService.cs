daa6108515efce36547b11d1d643a0d6894220e9
Tyuiu.KhudojberdievDB@outlook.com <Tyuiu.KhudojberdievDB@outlook.com>
2024-09-22 13:04:45 +0300

    Task6

---
+++ Tyuiu.KhudoiberdievDB.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,42 @@
+﻿using System.Numerics;
+
+namespace Tyuiu.KhudoiberdievDB.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+
+    {
+        public static object AdditionArray(int[] numbers)
+        {
+            var total = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                total = total + numbers[i];
+            }
+            return total;
+        }
+        public static object SubstractionArray(int[] numbers)
+        {
+            var tottal = 0;
+            int index = 0;
+            while (index < numbers.Length)
+            {
+                tottal = tottal - numbers[index];
+                index++;
+            }
+            return tottal;
+
+        }
+        public static object MultiplicationArray(int[] numbers)
+        {
+            var total = 1;
+            int index = 0;
+
+            while(index < numbers.Length)
+            {
+                total = total * numbers[index];
+                index++;
+            }
+            return total;
+        }
+    }
+}
---
+++ Tyuiu.KhudoiberdievDB.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KhudoiberdievDB.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.KhudoiberdievDB.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsAray = new int[] { 1, 2, 3, 4, 5, };
+            Console.WriteLine("Сумма элеметов массива = "+ DataService.AdditionArray(numsAray));
+            Console.WriteLine("Разность элементов массива =" +DataService.SubstractionArray(numsAray));
+            Console.WriteLine("Произведение эдементов массива ="+ DataService.MultiplicationArray(numsAray));
+            Console.ReadKey();
+
+        }
+    }
+}
