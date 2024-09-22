f1442f098fd0acc22760f988eed28365e186f4dd
Tyuiu.KhudojberdievDB@outlook.com <Tyuiu.KhudojberdievDB@outlook.com>
2024-09-22 17:20:06 +0300

    Task7

---
+++ Tyuiu.KhudoiberdievDB.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿namespace Tyuiu.KhudoiberdievDB.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] ResultArray = new int[5];
+            for (var i = 0 ; i < numOne.Length; i++)
+            {
+                ResultArray[i] = numOne[i] + numTwo[i];
+            }
+            return ResultArray;
+        
+        }
+    }
+}
