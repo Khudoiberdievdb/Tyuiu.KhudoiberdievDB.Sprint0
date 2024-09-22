e562858dd4d33600d357e13de5a8a90e22dfa693
Tyuiu.KhudojberdievDB@outlook.com <Tyuiu.KhudojberdievDB@outlook.com>
2024-09-22 10:38:28 +0300

    Task4

---
+++ Tyuiu.KhudoiberdievDB.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+using Tyuiu.KhudoiberdievDB.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.KhudoiberdievDB.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+        public void CheckedMiltiplicationValid()
+        { Assert.AreEqual(25, DataService.Multiplication(5, 5)); }
+        [TestMethod]
+        public void CheckedSubstractionValid()
+        {
+            Assert.AreEqual(0, DataService.Substraction(5, 5));
+        }
+        [TestMethod]
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(5, DataService.Division(25, 5));
+        }
+    }
+}
+        
+    
---
+++ Tyuiu.KhudoiberdievDB.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.KhudoiberdievDB.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.KhudoiberdievDB.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(5, 8));
+            Console.WriteLine(DataService.Multiplication(3, 8));
+            Console.WriteLine(DataService.Substraction(88, 4));
+            Console.WriteLine(DataService.Division(10,5 ));
+            Console.ReadKey();
+        }
+    }
+}
